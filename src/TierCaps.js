563176801193d59e8d8aa41f3f51e46a2872c586
Jussi <saila.jussi@gmail.com>
2025-02-06 21:39:11 +0200

    Caps change

--- src/TierCaps.js
+++ src/TierCaps.js
@@ -1,9 +1,9 @@
 const tierCaps = [
-	{ name: "Master", cap: 7575 },
-	{ name: "Elite", cap: 6840 },
-	{ name: "Rival", cap: 6470 },
-	{ name: "Challenger", cap: 6005 },
-	{ name: "Prospect", cap: 5175 },
+	{ name: "Master", cap: 7790 },
+	{ name: "Elite", cap: 6970 },
+	{ name: "Rival", cap: 6570 },
+	{ name: "Challenger", cap: 6065 },
+	{ name: "Prospect", cap: 5290 },
 ];
 
 export default tierCaps;
