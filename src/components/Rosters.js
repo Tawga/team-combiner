fdc14a195747344e2ad7a84a3a11cf81690e6c14
Jussi Säilä <saila.jussi@gmail.com>
2025-08-03 13:09:30 +0300

    Refactor Rosters component to enhance layout and improve readability

--- src/components/Rosters.js
+++ src/components/Rosters.js
@@ -3,20 +3,26 @@ import { Container, Card, Grid } from "@mui/material";
 import TeamCard from "./TeamCard";
 
 const Rosters = ({ possibleRosters, teamCap }) => {
-  return (
-    <Container maxWidth="lg">
-      <h3>Possible rosters: {possibleRosters.length}</h3>
-      <Grid container spacing={2}>
-        {possibleRosters.map((team, index) => (
-          <Grid key={index} item xs={6} sm={4} md={3}>
-            <Card>
-              <TeamCard players={team.players} cap={teamCap} cmv={team.totalCmv} />
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  );
+	return (
+		<Container maxWidth="lg">
+			<Card sx={{ marginBottom: 2, paddingLeft: 2, paddingRight: 2 }}>
+				<h3>Number of possible rosters: {possibleRosters.length}</h3>
+			</Card>
+			<Grid container spacing={2}>
+				{possibleRosters.map((team, index) => (
+					<Grid key={index} item xs={6} sm={4} md={3}>
+						<Card>
+							<TeamCard
+								players={team.players}
+								cap={teamCap}
+								cmv={team.totalCmv}
+							/>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Container>
+	);
 };
 
 export default Rosters;
