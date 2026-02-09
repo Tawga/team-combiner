5be1e3beefbb78dc95404007001a8567191418ea
Jussi Säilä <saila.jussi@gmail.com>
2026-02-09 21:01:49 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Combinations from "./features/TeamCombiner/TeamCombiner";
 import AppBarComponent from "./components/AppBarComponent";
 import HelpSection from "./components/HelpSection";
+import FooterComponent from "./components/FooterComponent";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: (
-			<div className="min-h-screen bg-gray-50 pb-10">
+			<div className="flex min-h-screen flex-col bg-gray-50">
 				<AppBarComponent title="Team Combiner" />
-				<div className="container mx-auto px-4 py-8 max-w-7xl">
+				<div className="container mx-auto px-4 py-8 max-w-7xl flex-grow">
 					<Combinations />
 					<HelpSection />
 				</div>
+				<FooterComponent />
 			</div>
 		),
 	},
